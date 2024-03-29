4faa48f135fcd5a11d13a3a58261d9f3f81d9fb5
fabiokleis <fabiohkrc@gmail.com>
2024-03-29 19:54:46 -0300

    implemented a basic file server

--- cmd/fserver-udp/main.go
+++ cmd/fserver-udp/main.go
@@ -1,56 +1,240 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	msg "fserver-udp/pkg/proto"
+	msg "fserver-udp/server/pkg/proto"
 	"net"
+	"os"
+	"sync"
 
+	"github.com/codingsince1985/checksum"
 	pb "google.golang.org/protobuf/proto"
 )
 
 const (
 	UDP_BIND_ADDRESS = "0.0.0.0:2224"
 	BUFF_SIZE        = 32
+	CHUNK_SIZE       = 255
 )
 
-func handleClient(socket net.Conn) {
+var workers = sync.Map{}
 
-	defer socket.Close()
+type Token struct {
+	Index    int
+	Received bool
+}
 
-	buffer := make([]byte, BUFF_SIZE)
+type TokenizedFile struct {
+	CheckSum string           // file sha256 checksum
+	Buffer   []byte           // file content
+	Tokens   map[string]Token // chunk tokens
+}
 
-	n, err := socket.Read(buffer)
+type Worker struct {
+	Socket        *net.UDPConn
+	Addr          net.Addr
+	PacketChannel chan []byte
+	File          TokenizedFile
+}
 
-	fmt.Println("read %d bytes", n)
+func (w *Worker) sendConfirmationPacket(result msg.Result, token string) {
+	confirmation := msg.Confirmation{Result: result, Token: token}
+	cBuffer, _ := pb.Marshal(&confirmation)
+	pBuffer := make([]byte, BUFF_SIZE+1)
+	pBuffer[0] = byte(msg.Verb_CONFIRMATION)
+	copy(pBuffer[1:], cBuffer)
+	(*w.Socket).WriteTo(cBuffer, w.Addr)
+}
 
-	if err != nil {
-		fmt.Printf("failed to read udp client socket %s, cause %s\n", socket.RemoteAddr(), err)
-		return
+func (w *Worker) confirmPacket(hash string) error {
+	token, ok := w.File.Tokens[hash]
+	if !ok {
+		return fmt.Errorf("token not found")
 	}
 
-	request := msg.Message{}
+	if token.Received {
+		return fmt.Errorf("token already confirmed")
+	}
 
-	if err := pb.Unmarshal(buffer[:], &request); err != nil {
-		fmt.Printf("failed to deacode buffer as request %s, cause %s", buffer, err)
+	w.File.Tokens[hash] = Token{Index: token.Index, Received: true}
+
+	return nil
+}
+
+func (w *Worker) sendFilePacket(hash string) {
+	token := w.File.Tokens[hash]
+	block := token.Index + CHUNK_SIZE
+
+	chunkBuffer, _ := pb.Marshal(
+		&msg.FileChunk{
+			Chunk:    w.File.Buffer[token.Index:block],
+			Token:    hash,
+			CheckSum: w.File.CheckSum,
+		},
+	)
+	pBuffer := make([]byte, BUFF_SIZE+1)
+	pBuffer[0] = byte(msg.Verb_RESPONSE)
+	copy(pBuffer[1:], chunkBuffer)
+
+	(*w.Socket).WriteTo(pBuffer, w.Addr)
+}
+
+func (w *Worker) sendFile() {
+	for hash := range w.File.Tokens {
+		w.sendFilePacket(hash)
+	}
+}
+
+func (w *Worker) tokenizeFile(buffer []byte) {
+	size := len(buffer)
+	tokens := make(map[string]Token)
+	for i := 0; i < size; i += CHUNK_SIZE {
+		hash := fmt.Sprintf("token_idx_%d", i)
+		tokens[hash] = Token{
+			Index:    i,
+			Received: false,
+		}
+	}
+
+	w.File = TokenizedFile{
+		Buffer: buffer,
+		Tokens: tokens,
+	}
+}
+
+func (w *Worker) readFile(buffer []byte) ([]byte, msg.Result, error) {
+
+	request := msg.RequestFile{}
+	if err := pb.Unmarshal(bytes.Trim(buffer, "\x00"), &request); err != nil {
+		return nil, msg.Result_INVALID_PACKET_FORMAT, err
+	}
+	fBuffer, err := os.ReadFile(request.FilePath)
+	if err != nil {
+		return nil, msg.Result_FILE_NOT_FOUND, err
+	}
+
+	sha256, err := checksum.SHA256sum(request.FilePath)
+	if err != nil {
+		return nil, msg.Result_ERROR_CHECK_SUM, err
+	}
+	fmt.Printf("sha256 checksum %v -> %s\n", request.FilePath, sha256)
+	w.File.CheckSum = sha256
+
+	return fBuffer, msg.Result_OK, nil
+}
+
+func (w *Worker) execute() {
+	defer func() {
+		close(w.PacketChannel)
+		workers.Delete(w.Addr)
+	}()
+
+	fmt.Println("executing new worker....")
+
+	for {
+		select {
+		case buffer := <-w.PacketChannel:
+			switch int(buffer[0]) {
+			case int(msg.Verb_REQUEST):
+				fileBuffer, result, err := w.readFile(buffer[1:]) // skip identification byte
+				switch result {
+				case msg.Result_INVALID_PACKET_FORMAT:
+					fmt.Printf("failed to decode buffer as protobuf message, cause %s\n", err)
+					w.sendConfirmationPacket(result, "")
+					return // exit worker
+
+				case msg.Result_FILE_NOT_FOUND:
+					fmt.Printf("failed to read file, cause %s\n", err)
+					w.sendConfirmationPacket(result, "")
+					return // exit worker
+
+				case msg.Result_ERROR_CHECK_SUM:
+					fmt.Printf("failed to generate sha256, cause %s\n", err)
+					w.sendConfirmationPacket(result, "")
+					return // exit worker
+
+				case msg.Result_OK:
+					fmt.Println("sending all file chunks packets....")
+					w.tokenizeFile(fileBuffer)
+					w.sendFile() // send all file chunks
+					fmt.Println("file sent")
+					break
+				default:
+					return // wtf
+				}
+
+			case int(msg.Verb_CONFIRMATION):
+				confirm := msg.Confirmation{}
+				if err := pb.Unmarshal(buffer[1:BUFF_SIZE], &confirm); err != nil {
+					fmt.Printf("failed to decode buffer as protobuf message, cause %s\n", err)
+					w.sendConfirmationPacket(msg.Result_INVALID_PACKET_FORMAT, "")
+					return // exit worker
+				}
+				switch confirm.Result {
+				case msg.Result_OK:
+					fmt.Println("received packet, token hash ", confirm.Token)
+					if err := w.confirmPacket(confirm.Token); err != nil {
+						fmt.Printf("cannot confirm packet, failed to find token hash %s, cause %s\n", confirm.Token, err)
+						w.sendConfirmationPacket(msg.Result_INVALID_TOKEN, confirm.Token)
+					}
+					break
+				case msg.Result_PACKET_MISS:
+					fmt.Println("resending packet, token hash ", confirm.Token)
+					w.sendFilePacket(confirm.Token) // resend missed packet
+					break
+				default:
+					return // wtf
+				}
+			}
+
+		default:
+			//fmt.Println("waiting packet.....")
+		}
+	}
+}
+
+func server(socket *net.UDPConn) {
+
+	defer (*socket).Close()
+
+	packet := make([]byte, BUFF_SIZE+1)
+	for {
+		n, addr, err := (*socket).ReadFrom(packet)
+		if err != nil {
+			fmt.Printf("failed to read udp client socket %s, cause %s\n", addr, err)
+			continue
+		}
+
+		fmt.Printf("address: %s, packet: %d %s\n", addr, n, packet)
+		if w, load := workers.Load(addr.String()); load {
+			worker := w.(*Worker)
+			worker.PacketChannel <- packet // send new packet
+			continue
+		}
+
+		// start a new worker
+		worker := &Worker{Socket: socket, Addr: addr, PacketChannel: make(chan []byte)}
+		workers.Store(addr.String(), worker)
+		go worker.execute()
+		worker.PacketChannel <- packet
 	}
 }
 
 func main() {
-	fmt.Println("start udp listener ", UDP_BIND_ADDRESS)
+	fmt.Println("starting udp listener", UDP_BIND_ADDRESS)
 
-	listener, err := net.Listen("udp", UDP_BIND_ADDRESS)
+	udpAddr, err := net.ResolveUDPAddr("udp", UDP_BIND_ADDRESS)
 
 	if err != nil {
-		fmt.Printf("failed to start udp listener %s, cause %s\n", UDP_BIND_ADDRESS, err)
+		fmt.Printf("failed to resolve udp address, cause %s\n", err)
+		os.Exit(1)
 	}
+	listener, err := net.ListenUDP("udp", udpAddr)
 
-	for {
-		client, err := listener.Accept()
-
-		if err != nil {
-			fmt.Printf("failed to handle udp client, cause %s\n", err)
-		}
-
-		go handleClient(client)
+	if err != nil {
+		fmt.Printf("failed starting udp listener, cause %s\n", err)
+		os.Exit(1)
 	}
+	server(listener)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module fserver-udp/server
 
 go 1.21.8
 
-require google.golang.org/protobuf v1.33.0
+require (
+	github.com/codingsince1985/checksum v1.3.0
+	google.golang.org/protobuf v1.33.0
+)
+
+require (
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+)
