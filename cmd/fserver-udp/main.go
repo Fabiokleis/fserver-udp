02fe455ed77ddd6ab718e851245c9377828ff5c6
fabiokleis <fabiohkrc@gmail.com>
2024-03-31 02:10:46 -0300

    dynamic packet size, set max but process total wrote

--- cmd/fserver-udp/main.go
+++ cmd/fserver-udp/main.go
@@ -9,14 +9,16 @@ import (
 
 const (
 	UDP_BIND_ADDRESS = "0.0.0.0:2224"
-	BUFF_SIZE        = 32
+	MAX_PACKET_SIZE  = 32
 )
 
 func monitor(workers map[string]*w.Worker) {
-	for addr, worker := range workers {
-		if (*worker).Done {
-			fmt.Printf("stop worker for %s", addr)
-			delete(workers, addr)
+	for {
+		for addr, worker := range workers {
+			if (*worker).Done {
+				fmt.Printf("[%s] exiting worker", addr)
+				delete(workers, addr)
+			}
 		}
 	}
 }
@@ -29,25 +31,30 @@ func server(socket *net.UDPConn) {
 
 	go monitor(workers)
 
-	packet := make([]byte, BUFF_SIZE)
+	packet := make([]byte, MAX_PACKET_SIZE)
 	for {
-		_, addr, err := (*socket).ReadFrom(packet)
+		n, addr, err := (*socket).ReadFrom(packet)
 		if err != nil {
 			fmt.Printf("failed to read udp client socket %s, cause %s\n", addr, err)
 			continue
 		}
 
-		fmt.Println("address", addr)
 		if w, ok := workers[addr.String()]; ok {
-			w.PacketChannel <- packet // send new packet
+			w.PacketChannel <- packet[:n] // send new packet
 			continue
 		}
 
 		// start a new worker
-		worker := &w.Worker{Socket: socket, Addr: addr, PacketChannel: make(chan []byte), Done: false}
+		worker := &w.Worker{
+			Socket:        socket,
+			Addr:          addr,
+			PacketChannel: make(chan []byte),
+			Waiting:       false,
+			Done:          false,
+		}
 		workers[addr.String()] = worker
 		go worker.Execute()
-		worker.PacketChannel <- packet
+		worker.PacketChannel <- packet[:n]
 	}
 }
 
